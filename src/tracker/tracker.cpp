8e8da9429bcb5f8495636f8241f724631dc9621a
Nick Villanueva <nickvill@mit.edu>
2018-04-13 22:30:39 -0400

    print typr

--- src/tracker/tracker.cpp
+++ src/tracker/tracker.cpp
@@ -16,6 +16,7 @@ Tracker::Tracker(const bool show_tracking) :
 
 void Tracker::Init_ObjDet(const cv::Mat& image, const BoundingBox& bbox_gt) {
   std::cout << "starting init!" << std::endl;
+  std::cout << typeid(image).name() << std::endl;
   std::cout << image.cols << " " << image.rows << std::endl;
   image_prev_ = image;
   std::cout << "set prev image" << std::endl;
