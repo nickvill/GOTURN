3090795fb64958350157ca85ca649cd16921ee57
Nick Villanueva <nickvill@mit.edu>
2018-04-16 14:03:21 -0400

    add include

--- src/tracker/tracker.h
+++ src/tracker/tracker.h
@@ -4,6 +4,7 @@
 #include <opencv/cv.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 #include "helper/bounding_box.h"
 #include "train/example_generator.h"
